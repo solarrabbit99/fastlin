dc98c7726571cb0025502458a44c70c1c08d355d
zhenghanlee <hanhan_1999@hotmail.com>
2025-02-24 02:03:33 +0800

    Improve mem_allocator ctor impl

--- include/commons/mem_alloc.h
+++ include/commons/mem_alloc.h
@@ -1,33 +1,40 @@
 #pragma once
 
-#include <vector>
-
 namespace fastlin {
 
 // Represents static sized memory_allocator for fast alloc and free
 template <typename T>
+  requires(sizeof(T) >= sizeof(void*))  // safe reinterpret_cast to free_node*
 struct memory_allocator {
-  memory_allocator(size_t size)
-      : data{(T*)malloc(sizeof(T) * size)}, free_list{size} {
-    for (int i = 0; i < size; ++i) free_list[i] = data + i;
-  }
+  struct free_node {
+    free_node* next;
+  };
+
+  explicit memory_allocator(size_t size)
+      : data{(T*)std::malloc(sizeof(T) * size)}, used{0}, free_list{nullptr} {}
 
   ~memory_allocator() { std::free(data); }
 
   T* alloc() {
-    T* top = free_list.back();
-    free_list.pop_back();
-    return top;
+    if (free_list) {  // reuse from free_list
+      T* slot = reinterpret_cast<T*>(free_list);
+      free_list = free_list->next;
+      return slot;
+    }
+    return &data[used++];  // allocate from block
   }
 
   void free(T* slot) {
     slot->~T();
-    free_list.push_back(slot);
+    auto* node = reinterpret_cast<free_node*>(slot);
+    node->next = free_list;
+    free_list = node;
   }
 
  private:
   T* data;
-  std::vector<T*> free_list;
+  size_t used;
+  free_node* free_list;
 };
 
 };  // namespace fastlin
